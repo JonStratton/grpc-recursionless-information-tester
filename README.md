6f83a11803bdc4d7854851d895c835fda45ba7a8
JonStratton <jgstratton@grpc.lan>
2022-04-13 13:48:25 +0000

    Clean up and README

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# grpc-recursionless-information-tester
+# grpc-reflectionless-information-tester
+A collection of POC scripts to enumerate and fuzz test grpc endpoints that dont use reflection. Currently these are just wrappers around [grpcurl](https://github.com/fullstorydev/grpcurl).
+
+**Scripts**
+- [**grit_endpoint_protofuzz.pl**](grit_endpoint_protofuzz.pl) - Fuzz a known endpoint / method / proto combo with a wordlist.
+
+**Future Scripts**
+- **grit_endpoint_methodfuzz.pl** - Fuzz a known endpoint / package for methods with a wordlist.
+- **grit_endpoint_protodump.pl** - Try to guess / dump a proto file from a known endpoint / method combo.
--- grit_endpoint_protofuzz.pl
+++ grit_endpoint_protofuzz.pl
@@ -1,6 +1,6 @@
 #!env perl
-# Just a POC for now; a wrapper around grpcurl.
-# perl grit_endpoint_protofuzz.pl -p ~/fuzzdb/attack/all-attacks/all-attacks-unix.txt
+# https://github.com/JonStratton/grpc-reflectionless-information-tester
+# Fuzz a known endpoint / method / proto combo with a wordlist.
 
 use strict;
 use warnings;
@@ -8,27 +8,31 @@ use threads;
 use Getopt::Std;
 use Digest::MD5 qw(md5_base64);
 use IPC::Open2;
-
-my $THREADS = 10;
-my $DATA = '{"name":"_PAYLOAD1_"}';
-# Formatted to take data from standard in.
-my $GRPCURL = 'grpcurl -plaintext -proto ./helloworld.proto -d @ localhost:50051 helloworld.Greeter/SayHello';
+use Time::HiRes qw(time);
 
 # Command line params
 my %opts = ();
-getopt('p', \%opts);
-my @payload_files = split( /,\s*/, $opts{'p'} );
+getopt('gdpt', \%opts);
+
+my @PAYLOAD_FILES = defined($opts{'p'}) ? split( /,\s*/, $opts{'p'} ) : ();
+my $THREADS = defined($opts{'t'}) ? $opts{'t'} : 10;
+my $DATA    = $opts{'d'};
+my $GRPCURL = $opts{'g'};
+if (!($THREADS and $DATA and $GRPCURL)) {
+   print "$0 -p ~/fuzzdb/attack/all-attacks/all-attacks-unix.txt -d '{\"name\":\"_PAYLOAD_\"}' -g 'grpcurl -plaintext -proto ./helloworld.proto -d @ localhost:50051 helloworld.Greeter/SayHello'\n";
+   exit(1);
+}
 
 sub main {
-   # Load Payloads and break down by threads. Looks like $payloads[thread#]{'PAYLOAD#'}
-   my @threads_payloads = load_payloads(@payload_files);
+   # Load Payloads and break down by threads. Looks like $payloads[thread#]
+   my @threads_payloads = load_payloads(@PAYLOAD_FILES);
 
    # Process workload
    my @threads = ();
    foreach my $thread_num (0..($THREADS-1)) {
       next unless defined($threads_payloads[$thread_num]);
-      my %payloads = %{$threads_payloads[$thread_num]};
-      push(@threads, threads->create('process_chunk', $DATA, \%payloads));
+      my @payloads = @{$threads_payloads[$thread_num]};
+      push(@threads, threads->create('process_chunk', $DATA, \@payloads));
    }
 
    # Wait for all the threads to return
@@ -37,41 +41,36 @@ sub main {
    }
 }
 
-# TODO: split our this read from the thread breakdown. 
+# Load a wordlist and break it down by thread
 sub load_payloads {
    my @payload_files = @_;
    my @payloads = ();
-   my $payload_num = 1;
    foreach my $payload_file (@payload_files) {
-      my $payload_name = sprintf('_PAYLOAD%d_', $payload_num);
       if (open(my $fh, '<', $payload_file)) {
          my $thread_num = 0;
          while(my $line = <$fh>) {
             chomp($line);
-            push(@{$payloads[$thread_num]{$payload_name}}, $line);
+            push(@{$payloads[$thread_num]}, $line);
             $thread_num = ($thread_num+1) % $THREADS;
          }
          close($fh);
       }
-      $payload_num++;
    }
    return(@payloads);
 }
 
-# TODO: thread breakdown should look like $payloads[$thread_num][] = ({P1 => blah, P2 = blah}, { etc }) so we can deal with multi payloads
-
+# Replace "_PAYLOAD_" in DATA for each payload in our wordlist, then send it to grpc_request().
 sub process_chunk {
    my ($data, $payloads_ref) = @_;
-   foreach my $payload_key (keys(%{$payloads_ref})) {
-      foreach my $payload (@{${$payloads_ref}{$payload_key}}) {
-         $payload =~ s/"/\\"/g; # Escape " in the payload so it doesnt mess with our json
-         my $data_new = $data;
-         $data_new =~ s/$payload_key/$payload/g;
-         grpc_request($data_new);
-      }
+   foreach my $payload (@{$payloads_ref}) {
+      $payload =~ s/"/\\"/g; # Escape " in the payload so it doesnt mess with our json
+      my $data_new = $data;
+      $data_new =~ s/_PAYLOAD_/$payload/g;
+      grpc_request($data_new);
    }
 }
 
+# Open grpcurl as pipe for both writing (so we can send our payloads in in a raw form) and for reading. Dump the output in a format we can log.
 sub grpc_request {
    my ($data) = @_;
    my $request_hash = md5_base64($data);
@@ -87,8 +86,8 @@ sub grpc_request {
 
    printf("%s|payload: %s\n", $request_hash, $data);
    printf("%s|return: %s\n", $request_hash, $line);
-   printf("%s|time: %d\n", $request_hash, (time - $start_time));
-
+   printf("%s|time: %f\n", $request_hash, (time - $start_time));
 }
 
 main();
+exit(0);
